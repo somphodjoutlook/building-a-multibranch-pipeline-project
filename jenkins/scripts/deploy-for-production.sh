bdcce40154f0fadf043fd1959a19c4175b4a8d18
somphod jaroensiri <somphodj@outlook.com>
2023-05-24 18:43:31 +0700

    port 5100

--- jenkins/scripts/deploy-for-production.sh
+++ jenkins/scripts/deploy-for-production.sh
@@ -29,11 +29,11 @@ echo 'of the previously run process (i.e. "serve") and writes this value to'
 echo 'the file ".pidfile".'
 set -x
 # serve -s build -l 5100 &
-./node_modules/serve/build/main.js -s build -l 3000 &
+./node_modules/serve/build/main.js -s build -l 5100 &
 echo $! > .pidfile
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost:5000 to see your Node.js/React application in action.'
-echo '(This is why you specified the "args ''-p 5000:5000''" parameter when you'
+echo 'Visit http://localhost:5100 to see your Node.js/React application in action.'
+echo '(This is why you specified the "args ''-p 5100:5100''" parameter when you'
 echo 'created your initial Pipeline as a Jenkinsfile.)'
